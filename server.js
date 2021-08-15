d9afe69788ec2b3e88119e9de75e11b401d14f97
horlash-Dev <cube6029@gmail.com>
2021-08-15 12:28:00 -0700

    heroku routing

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(serve(__dirname + "/dist"));
+app.get(/.*/, function(res,req){
+	res.sendfile(__dirname + "/dist/index.html")
+})
 app.listen(port)
